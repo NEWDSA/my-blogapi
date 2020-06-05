52f771e2ffa86f207eb639f641c82e85fdfcf45c
NEWDAS <953627561@qq.com>
2020-06-05 13:04:48 +0800

    更新url

--- blog-api/server.js
+++ blog-api/server.js
@@ -2,21 +2,21 @@ var express = require('express');
 var app = express();
 
 const sql = require('mssql')
-//standard tedious config object : http://tediousjs.github.io/tedious/api-connection.html#function_newConnection
+    //standard tedious config object : http://tediousjs.github.io/tedious/api-connection.html#function_newConnection
 var config = {
-  user: 'sa',
-  password: '123',
-  server: 'localhost',
-  port: 1433,
-  database: 'Luciano_DB',
-  options: {
-    encrypt: false,// 是否加密，本地为false, 部署后为true 
-  },
-  pool: {
-    min: 0,
-    max: 10,
-    idleTimeoutMillis: 3000
-  }
+    user: 'sa',
+    password: '123',
+    server: 'localhost',
+    port: 1433,
+    database: 'Luciano_DB',
+    options: {
+        encrypt: false, // 是否加密，本地为false, 部署后为true 
+    },
+    pool: {
+        min: 0,
+        max: 10,
+        idleTimeoutMillis: 3000
+    }
 }
 
 // app.post('/upload', function (req, res) {
@@ -65,64 +65,80 @@ var config = {
 //     })
 //   })
 // });
-app.get('/note/', function (req, res) {
-  sql.connect(config).then(() => {
-    return sql.query`select * from Luciano_Note`
-  }).then(result => {
-    var data = result.recordset;
-    // res.send(data);
-    // res.render('albums', {
-    //   title: '我的相册',
-    //   id: '2',
-    //   data: data
-    // })
-    // conn.close();
-    res.send(data);
-  }).catch(err => {
-    // ... error checks
-  })
-  sql.on('error', err => {
-    // ... error handler
-  })
+app.get('/note/', function(req, res) {
+    sql.connect(config).then(() => {
+        return sql.query `select * from Luciano_Note`
+    }).then(result => {
+        var data = result.recordset;
+        // res.send(data);
+        // res.render('albums', {
+        //   title: '我的相册',
+        //   id: '2',
+        //   data: data
+        // })
+        // conn.close();
+        res.send(data);
+    }).catch(err => {
+        // ... error checks
+    })
+    sql.on('error', err => {
+        // ... error handler
+    })
 });
-app.get('/photo',function(req,res){
-  sql.connect(config).then(()=>{
-    return sql.query`select * from Luciano_Photo`
-  }).then(result=>{
-    var data=result.recordset;
-    res.send(data);
-  }).catch(err=>{
-      console.log(err);
-  });
-   sql.on('error',err=>{
-     console.log(err);
-   })
+app.get('/albums', function(req, res) {
+    sql.connect(config).then(() => {
+        return sql.query `select * from albums`
+    }).then(result => {
+        var data = result.recordset;
+        res.send(data);
+    }).catch(err => {
+        console.log(err);
+    });
+    sql.on('error', err => {
+        console.log(err);
+    })
 });
 
-app.get('/search:userid', function (req, res) {
- var par=req.params.userid.replace(':','');
-  sql.connect(config, function (err) {
-    if (err) {
-      console.log(err);
-    }
-    var request = new sql.Request();
-    var test=par;
-    request.input('content', sql.NVarChar, test);
-    console.log(test)
-    request.execute('Luciano_TS', function (err, recordsets) {
-      if (err) {
+app.get('/photo/:id', function(req, res) {
+   const id= req.params;
+   console.log(id);
+    sql.connect(config).then(() => {
+        return sql.query `select * from Luciano_Photo`
+    }).then(result => {
+        var data = result.recordset;
+        res.send(data);
+    }).catch(err => {
         console.log(err);
-      }
-      var str = JSON.stringify(recordsets);
-      res.send(recordsets);
-      console.log(recordsets);
+    });
+    sql.on('error', err => {
+        console.log(err);
     })
-  })
+});
+
+app.get('/search:userid', function(req, res) {
+    var par = req.params.userid.replace(':', '');
+    sql.connect(config, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        var request = new sql.Request();
+        var test = par;
+        request.input('content', sql.NVarChar, test);
+        console.log(test)
+        request.execute('Luciano_TS', function(err, recordsets) {
+            if (err) {
+                console.log(err);
+            }
+            var str = JSON.stringify(recordsets);
+            res.send(recordsets);
+            console.log(recordsets);
+        })
+    })
 });
 
 
 
 app.listen(8000, () => {
-  console.log('app listening on 8000');
-  // console.log(path.join(__dirname, "public", "file.html"));
+    console.log('app listening on 8000');
+    // console.log(path.join(__dirname, "public", "file.html"));
 })
